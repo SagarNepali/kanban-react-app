6ab17c06c45060546687e61a086c8c1fa7d8aee2
SagarNepali <nepalisagar23@gmail.com>
2018-03-12 13:49:32 +0545

    Working with states in card.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,14 +5,30 @@ import React, {Component} from 'react';
 import CheckList from '../CheckList/CheckList';
 
 class Card extends Component{
+
+	constructor() {
+		super(...arguments);
+		this.state = {
+			showDetails: false
+		};
+	}
+
 	render(){
-		return(
-			<div className="card">
-				<div className="card__title">{this.props.title}</div>
+
+		let cardDetails;
+		if (this.state.showDetails) {
+			cardDetails = (
 				<div className="card__details">
 					{this.props.description}
-					<CheckList cardId={this.props.id} tasks={this.props.tasks}/>
+					<CheckList cardId={this.props.id} tasks={this.props.tasks} />
 				</div>
+			);
+		};
+
+		return(
+			<div className="card">
+				<div className="card__title" onClick={()=>this.setState({showDetails: false})}>{this.props.title}</div>
+				{cardDetails}
 			</div>
 		);
 	}
